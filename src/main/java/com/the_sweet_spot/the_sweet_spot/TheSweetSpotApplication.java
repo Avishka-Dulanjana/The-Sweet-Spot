db79975912c7f23318dcc965294659d16875c6be
Avishka-Dulanjana <avishkadulanjana98@gmail.com>
2024-05-28 18:53:13 +0530

    first commit

---
+++ src/main/java/com/the_sweet_spot/the_sweet_spot/TheSweetSpotApplication.java
@@ -0,0 +1,13 @@
+package com.the_sweet_spot.the_sweet_spot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSweetSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSweetSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/the_sweet_spot/the_sweet_spot/TheSweetSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the_sweet_spot.the_sweet_spot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSweetSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
